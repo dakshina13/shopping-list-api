cba2796e60ec177a73939083a7c912f5829408d3
dakshinamurthy <dakshinamurthy13@gmail.com>
2021-08-04 13:46:28 +0530

    Back end to connect to Mongodb atlas

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+
+const app = express();
+
+const routes = require("./mongoose");
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  );
+  next();
+});
+
+app.get("/shopping", routes.getItems);
+
+app.post("/add-item", routes.additem);
+
+app.put("/edit-item", routes.updateItem);
+
+app.delete("/delete-item", routes.deleteItem);
+app.listen(5000);
---
+++ mongoose.js
@@ -0,0 +1,74 @@
+const mongoose = require("mongoose");
+
+const Item = require("./models/item");
+
+mongoose
+  .connect(
+    "mongodb+srv://dak:abcd1234@cluster0.z70im.mongodb.net/test?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+  
+const addItem = async (req, res, next) => {
+  if (req.name == null || req.quantity == null) {
+    return res.status(440).json({ message: "Name and quantity is required!" });
+  }
+  const item = new Item({
+    name: req.body.name,
+    quantity: req.body.quantity,
+  });
+  const result = await item.save();
+  res.json(result);
+};
+
+
+const getItems = async (req, res, next) => {
+  const items = await Item.find().exec();
+  res.json(items);
+};
+
+const updateItem = async (req, res, next) => {
+  if (req.name == null || req.quantity == null || req.id == null) {
+    return res
+      .status(440)
+      .json({ message: "Name, quantity and id is required!" });
+  }
+  const item = await Item.findOne({ _id: req.body.id });
+  if (item) {
+    await item.updateOne({
+      name: req.body.name,
+      quantity: req.body.quantity,
+    });
+    const updatedItem = await Item.findOne({ _id: req.body.id });
+    //console.log(item);
+    return res.json(updatedItem);
+  }
+  res.status(400).json({
+    message: "Invalid id.",
+  });
+};
+
+
+const deleteItem = async (req, res, next) => {
+  if (req.id == null) {
+    return res.status(440).json({ message: "Id is required!" });
+  }
+  const item = await Item.findOne({ _id: req.body.id });
+  if (item) {
+    await item.deleteOne();
+    return res.json(item);
+  }
+  res.status(400).json({
+    message: "Invalid id.",
+  });
+};
+
+exports.additem = addItem;
+exports.getItems = getItems;
+exports.updateItem = updateItem;
+exports.deleteItem = deleteItem;
